e29661f1d137be8923356dbd4b0faa4f1a7f419a
John Slowik <john@jslowik.com>
2022-09-23 12:02:41 -0500

    Navigation actually works

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import './App.scss';
-import {Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
     return (
-        <Routes>
-            <Route path='/' element={<AllMeetupsPage/>}></Route>
-            <Route path='/new-meetup' element={<NewMeetupPage/>}></Route>
-            <Route path='/favorites' element={<FavoritesPage/>}></Route>
-        </Routes>
+        <BrowserRouter>
+            <MainNavigation/>
+            <Routes>
+                <Route path='/' element={<AllMeetupsPage/>}></Route>
+                <Route path='/new-meetup' element={<NewMeetupPage/>}></Route>
+                <Route path='/favorites' element={<FavoritesPage/>}></Route>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
