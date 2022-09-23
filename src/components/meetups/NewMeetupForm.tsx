77e8d9d27864f92ccb427db1e9b04e38aec5a475
John Slowik <john@jslowik.com>
2022-09-23 16:37:53 -0500

    Initial form

--- src/components/meetups/NewMeetupForm.tsx
+++ src/components/meetups/NewMeetupForm.tsx
@@ -1,25 +1,48 @@
 import style from "./NewMeetupForm.module.scss"
 import Card from "../ui/card/Card";
+import {useRef} from "react";
 
 function NewMeetupForm() {
+    const titleInputRef = useRef<HTMLInputElement>(null);
+    const imageInputRef = useRef<HTMLInputElement>(null);
+    const addressInputRef = useRef<HTMLInputElement>(null);
+    const descriptionInputRef = useRef<HTMLTextAreaElement>(null);
+
+    function submitHandler(event) {
+        event.preventDefault();
+        const enteredTitle = titleInputRef.current.value;
+        const enteredImageUrl = imageInputRef.current.value;
+        const enteredAddress = addressInputRef.current.value;
+        const enteredDescription = descriptionInputRef.current.value;
+
+        const meetupData = {
+            title: enteredTitle,
+            image: enteredImageUrl,
+            address: enteredAddress,
+            description: enteredDescription
+        };
+
+        console.log(meetupData);
+    }
+
     return (
         <Card>
-            <form className={style.form}>
+            <form className={style.form} onSubmit={submitHandler}>
                 <div className={style.control}>
                     <label htmlFor="title">Meetup Title</label>
-                    <input type="text" required id="title"/>
+                    <input type="text" required id="title" ref={titleInputRef}/>
                 </div>
                 <div className={style.control}>
                     <label htmlFor="image">Meetup Image</label>
-                    <input type="url" required id="image"/>
+                    <input type="url" required id="image" ref={imageInputRef}/>
                 </div>
                 <div className={style.control}>
                     <label htmlFor="address">Meetup Address</label>
-                    <input type="text" required id="address"/>
+                    <input type="text" required id="address" ref={addressInputRef}/>
                 </div>
                 <div className={style.control}>
                     <label htmlFor="description">Meetup Description</label>
-                    <textarea id="description" required rows={5}></textarea>
+                    <textarea id="description" required rows={5} ref={descriptionInputRef}></textarea>
                 </div>
                 <div className={style.actions}>
                     <button>Add Meetup</button>
