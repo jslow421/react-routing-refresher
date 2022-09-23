6c60088a87335866774bf3c10fa12cc0972559b8
John Slowik <john@jslowik.com>
2022-09-23 11:52:39 -0500

    Routing added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
+import {Route, Routes} from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <Routes>
+            <Route path='/' element={<AllMeetupsPage/>}></Route>
+            <Route path='/new-meetup' element={<NewMeetupPage/>}></Route>
+            <Route path='/favorites' element={<FavoritesPage/>}></Route>
+        </Routes>
+    );
 }
 
 export default App;
